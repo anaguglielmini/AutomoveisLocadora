3e0d276c3f4e5e7a03c848356ac8859959be1b97
AugustoCarvalho <gutinho102004@gmail.com>
2021-03-04 15:02:04 -0300

    progresso

--- AutomoveisLocadora/AutomoveisLocadora/Veiculo.cs
+++ AutomoveisLocadora/AutomoveisLocadora/Veiculo.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,11 +9,33 @@ namespace AutomoveisLocadora
 {
     class Veiculo
     {
-        public string placaV, descV, marcaV, modeloV;
+        public string PlacaV, DescV, MarcaV, ModeloV;
         public int Id;
-        public double precoV;
-        public bool statusV;
-        public DateTime retiradaV, devolucaoV = DateTime.Now.Date;
+        public double PrecoV;
+        public bool StatusV;
+        public DateTime RetiradaV, DevolucaoV = DateTime.Now.Date;
+        public void Adicionar(string placaV, string descV, string marcaV, string modeloV, double precoV)
+        {
+            this.Id = 0;
+            this.PlacaV = placaV;
+            this.DescV = descV;
+            this.MarcaV = marcaV;
+            this.ModeloV = modeloV;
+            this.PrecoV = precoV;
+            this.StatusV = false;
+        }
+        public void Carregar(int id)
+        {
+            MySqlDataReader reader = DB.Read("select id_veiculo, placa_veiculo, desc_veiculo, marca_veiculo, modelo_veiculo, preco_veiculo, stts_veiculo, data_emprestimo, data_devolucao from cad_veiculo where cad_veiculo.id = ?id", new MySqlParameter[] {new MySqlParameter("id", id)});
 
+            if (reader != null)
+            {
+                while (reader.Read())
+                {
+                    this.Id = reader.GetInt32(0);
+
+                }
+            }
+        }
     }
 }
